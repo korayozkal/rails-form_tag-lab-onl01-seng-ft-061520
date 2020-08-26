8573db1fc696b783e6f28b1a33c057ea6fa5f1d1
Koray Ozkal <koryozkal@gmail.com>
2020-08-26 07:20:22 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ def new
   
 def create
   Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-  redirect_to new_student_path
+  redirect_to :new_student_path
 end
 
 end
