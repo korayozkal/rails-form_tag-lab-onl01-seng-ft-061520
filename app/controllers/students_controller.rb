dc1570cd5024aff9147b93d932a53188d799a6aa
Koray Ozkal <koryozkal@gmail.com>
2020-08-26 07:24:16 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,8 +8,6 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-def new
-  end
   
 def create
   Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
